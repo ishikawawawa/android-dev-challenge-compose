74f3a30e0f2813e7b9de4bdf4e28dd3765895233
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2021-02-26 03:05:51 +0900

    フォーマッタを実行

--- app/src/main/java/com/example/androiddevchallenge/views/home/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/views/home/HomeScreen.kt
@@ -1,6 +1,29 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.views.home
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
@@ -21,51 +44,52 @@ fun HomeScreen(
 ) {
     val scrollState = rememberScrollState()
 
-    Scaffold(content = {
-        Column(
-            modifier = Modifier
-                .verticalScroll(scrollState)
-                .fillMaxHeight(),
-        ) {
-            // todo そのうちどうにかする
-            repeat(10) {
-                Spacer(modifier = Modifier.height(4.dp))
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(1f)
-                        .padding(horizontal = 4.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
+    Scaffold(
+        content = {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(scrollState)
+                    .fillMaxHeight(),
+            ) {
+                // todo そのうちどうにかする
+                repeat(10) {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(1f)
+                            .padding(horizontal = 4.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
 
-                    thumbnail(dummy.get(1), Modifier.weight(0.8f)) { dogId ->
-                        navController.navigate("detail/${dogId}")
+                        thumbnail(dummy.get(1), Modifier.weight(0.8f)) { dogId ->
+                            navController.navigate("detail/$dogId")
+                        }
+                        Spacer(Modifier.width(4.dp))
+                        thumbnail(dummy.get(2), Modifier.weight(0.8f)) { dogId ->
+                            navController.navigate("detail/$dogId")
+                        }
                     }
-                    Spacer(Modifier.width(4.dp))
-                    thumbnail(dummy.get(2), Modifier.weight(0.8f)) { dogId ->
-                        navController.navigate("detail/${dogId}")
-                    }
-                }
-                Spacer(modifier = Modifier.height(4.dp))
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(1f)
-                        .padding(horizontal = 4.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    thumbnail(dummy.get(3), Modifier.weight(0.8f)) { dogId ->
-                        navController.navigate("detail/${dogId}")
-                    }
-                    Spacer(Modifier.width(4.dp))
-                    thumbnail(dummy.get(4), Modifier.weight(0.8f)) { dogId ->
-                        navController.navigate("detail/${dogId}")
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(1f)
+                            .padding(horizontal = 4.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        thumbnail(dummy.get(3), Modifier.weight(0.8f)) { dogId ->
+                            navController.navigate("detail/$dogId")
+                        }
+                        Spacer(Modifier.width(4.dp))
+                        thumbnail(dummy.get(4), Modifier.weight(0.8f)) { dogId ->
+                            navController.navigate("detail/$dogId")
+                        }
                     }
                 }
             }
         }
-    })
-
+    )
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
--- app/src/main/java/com/example/androiddevchallenge/views/home/Thumbnail.kt
+++ app/src/main/java/com/example/androiddevchallenge/views/home/Thumbnail.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.views.home
 
 import androidx.compose.foundation.Image
@@ -19,7 +34,8 @@ fun thumbnail(dog: Dog, modifier: Modifier, onPress: (dogId: Int) -> Unit) {
         Box(
             Modifier.clickable(
                 onClick = { onPress(dog.id) }
-            )) {
+            )
+        ) {
             Image(
                 painter = painterResource(dog.thumbnail),
                 contentDescription = "${dog.name}",
